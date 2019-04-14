619fa5edc3626f4be8120a6868d67a7e8c62abed
Nbiba Bedis <bedisnbiba@gmail.com>
2019-04-14 18:04:11 +0100

    ai!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod math;
 use math::{mse, Activfn, Math};
 
+const LEARNING_RATE: f64 = 0.1;
+const EPOCH: usize = 1000;
+
 struct NeuralNet {
     n1: Neuron,
     n2: Neuron,
@@ -14,46 +17,114 @@ impl NeuralNet {
             n3: Neuron::new(vec![0.0, 1.0], 0.0),
         }
     }
-    fn feedforward(&self, inputs: &[f64]) -> f64 {
+    fn feedforward(&mut self, inputs: &[f64]) -> f64 {
         let h1 = self.n1.feedforward(&inputs);
         let h2 = self.n2.feedforward(&inputs);
 
-        self.n3.feedforward(&[h1, h2])
+        let h1h2 = vec![h1, h2];
+        self.n3.feedforward(&h1h2)
 
         //dbg!(out);
         //out
     }
+    fn train(&mut self, data: Vec<Vec<f64>>, all_y_true: Vec<f64>) {
+        for _ in 0..EPOCH {
+            let mut all_y_preds = Vec::new();
+            for (x, y_true) in data.iter().zip(all_y_true.iter()) {
+                let y_pred = self.feedforward(x);
+                all_y_preds.push(y_pred);
+
+                self.backpropagate(y_pred, *y_true);
+            }
+            let loss = Self::calculate_loss(&all_y_preds, &all_y_true);
+            println!("{}", loss);
+        }
+    }
+    fn backpropagate(&mut self, y_pred: f64, y_true: f64) {
+        let d_l_d_ypred = -2. * (y_true - y_pred);
+
+        let n3 = self.n3.backpropagate(d_l_d_ypred, None);
+        let n2 = self.n2.backpropagate(d_l_d_ypred, Some(self.n3.weights[0]));
+        let n1 = self.n1.backpropagate(d_l_d_ypred, Some(self.n3.weights[1]));
+
+        self.n3 = n3.unwrap();
+        self.n2 = n2.unwrap();
+        self.n1 = n1.unwrap();
+    }
+    fn calculate_loss(all_y_preds: &[f64], all_y_true: &[f64]) -> f64 {
+        mse(
+            0,
+            all_y_preds.len(),
+            0.25,
+            all_y_true
+                .iter()
+                .zip(all_y_preds)
+                .map(|(a, b)| [*a, *b].to_vec())
+                .collect::<Vec<Vec<f64>>>(),
+        )
+    }
 }
+
+#[derive(Default)]
 struct Neuron {
     weights: Vec<f64>,
     bias: f64,
+    input: Option<Vec<f64>>,
+    output: Option<f64>,
+    sum: Option<f64>,
 }
 
 impl Neuron {
     fn new(weights: Vec<f64>, bias: f64) -> Self {
-        Self { weights, bias }
+        Self {
+            weights,
+            bias,
+            input: None,
+            output: None,
+            sum: None,
+        }
     }
-    fn feedforward(&self, inputs: &[f64]) -> f64 {
+    fn feedforward(&mut self, inputs: &[f64]) -> f64 {
+        self.input = Some(inputs.to_vec());
+
         let result = self.weights.dot(inputs) + self.bias;
+
+        self.sum = Some(result);
         //dbg!(result);
+        let out = Activfn::sigmoid(result);
+        self.output = Some(out);
+
         Activfn::sigmoid(result)
     }
+    fn backpropagate(&mut self, d_l_d_ypred: f64, out_weight: Option<f64>) -> Option<Neuron> {
+        let d_ypred_d_b = Activfn::deriv_sigmoid(self.sum?);
+        let d_ypred_d_w: Vec<f64> = self.input.clone()?.mul_x(d_ypred_d_b);
+
+        let d_ypred_d_h = if let Some(out_weight) = out_weight {
+            out_weight * d_ypred_d_b
+        } else {
+            1.
+        };
+
+        let weights = self.weights.minus_vec(
+            &d_ypred_d_w
+                .mul_x(d_l_d_ypred)
+                .mul_x(LEARNING_RATE)
+                .mul_x(d_ypred_d_h),
+        );
+
+        let bias = self.bias - d_ypred_d_b * d_l_d_ypred * LEARNING_RATE * d_ypred_d_h;
+
+        Some(Neuron {
+            weights,
+            bias,
+            ..Default::default()
+        })
+    }
 }
 
-fn main() {
-    let v = [[1.0, 0.0], [0.0, 0.0], [0.0, 0.0], [1.0, 0.0]];
-    let r = mse(
-        0,
-        4,
-        0.25,
-        &v.iter().map(|vv| &vv[..]).collect::<Vec<&[f64]>>(),
-    );
-    dbg!(r);
-    test_neural_net();
-}
-
-fn test_neural_net() {
-    let neural_net = NeuralNet::new();
+fn _test_neural_net() {
+    let mut neural_net = NeuralNet::new();
     let inputs = vec![2.0, 3.0];
 
     let _out = neural_net.feedforward(&inputs);
@@ -63,9 +134,9 @@ fn _test_neuron() {
     let weights = vec![0.0, 1.0];
     let bias = 0.0;
 
-    let n1 = Neuron::new(weights.clone(), bias);
-    let n2 = Neuron::new(weights.clone(), bias);
-    let n3 = Neuron::new(weights.clone(), bias);
+    let mut n1 = Neuron::new(weights.clone(), bias);
+    let mut n2 = Neuron::new(weights.clone(), bias);
+    let mut n3 = Neuron::new(weights.clone(), bias);
 
     let inputs = vec![2.0, 3.0];
 
@@ -75,4 +146,18 @@ fn _test_neuron() {
     let out = n3.feedforward(&[out1, out2]);
 
     dbg!(out);
+}
+
+fn main() {
+    let data = vec![
+        vec![-2., -1.],
+        vec![25., 6.],
+        vec![17., 4.],
+        vec![-15., -6.],
+    ];
+
+    let all_y_trues = vec![1., 0., 0., 1.];
+
+    let mut network = NeuralNet::new();
+    network.train(data, all_y_trues);
 }
--- src/math.rs
+++ src/math.rs
@@ -1,10 +1,36 @@
 pub trait Math {
     fn dot(&self, other: &[f64]) -> f64;
+    fn mul_x(&self, other: f64) -> Vec<f64>;
+    fn mul_vec(&self, other: &[f64]) -> Vec<f64>;
+    fn minus_vec(&self, other: &[f64]) -> Vec<f64>;
 }
 
 impl Math for Vec<f64> {
     fn dot(&self, other: &[f64]) -> f64 {
         self.iter().zip(other).fold(0.0, |acc, (x, y)| acc + x * y)
+    }
+    fn mul_x(&self, other: f64) -> Vec<f64> {
+        self.iter().map(|v| v * other).collect()
+    }
+    fn mul_vec(&self, other: &[f64]) -> Vec<f64> {
+        self.iter().zip(other).map(|(a, b)| a * b).collect()
+    }
+    fn minus_vec(&self, other: &[f64]) -> Vec<f64> {
+        self.iter().zip(other).map(|(a, b)| a - b).collect()
+    }
+}
+impl Math for &[f64] {
+    fn dot(&self, other: &[f64]) -> f64 {
+        self.iter().zip(other).fold(0.0, |acc, (x, y)| acc + x * y)
+    }
+    fn mul_x(&self, other: f64) -> Vec<f64> {
+        self.iter().map(|v| v * other).collect()
+    }
+    fn mul_vec(&self, other: &[f64]) -> Vec<f64> {
+        self.iter().zip(other).map(|(a, b)| a * b).collect()
+    }
+    fn minus_vec(&self, other: &[f64]) -> Vec<f64> {
+        self.iter().zip(other).map(|(a, b)| a - b).collect()
     }
 }
 
@@ -13,14 +39,18 @@ impl Activfn {
     pub fn sigmoid(x: f64) -> f64 {
         1.0 / (1.0 + (-x).exp())
     }
+    pub fn deriv_sigmoid(x: f64) -> f64 {
+        let fx = Activfn::sigmoid(x);
+        fx * (1. - fx)
+    }
 }
 
-pub fn mse(i: usize, n: usize, a: f64, input: &[&[f64]]) -> f64 {
+pub fn mse(i: usize, n: usize, a: f64, input: Vec<Vec<f64>>) -> f64 {
     let f = |x: &[f64]| (x[0] - x[1]).powf(2.0);
     a * sum(i, n, f, input)
 }
 
-fn sum(i: usize, n: usize, f: impl Fn(&[f64]) -> f64, inputs: &[&[f64]]) -> f64 {
+fn sum(i: usize, n: usize, f: impl Fn(&[f64]) -> f64, inputs: Vec<Vec<f64>>) -> f64 {
     let mut s = 0.0;
     for input in inputs.iter().take(n).skip(i) {
         s += f(input);
